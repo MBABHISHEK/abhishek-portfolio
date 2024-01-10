b71f33bb58bb9d0c35910a2943c0f54401c86e01
ABHI <mba384420@gmail.com>
2024-01-10 13:44:27 +0530

    added home and projects

---
+++ my-portfolio/src/components/Home.js
@@ -0,0 +1,14 @@
+// components/Home.js
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to My Portfolio</h2>
+      <p>This is the home section of my portfolio.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ my-portfolio/src/components/Projects.js
@@ -0,0 +1,18 @@
+// components/Projects.js
+
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h2>My Projects</h2>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
