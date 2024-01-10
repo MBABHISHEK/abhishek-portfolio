93ef617f1ffb72412cec0e9355efb2e8eacafd3d
ABHI <mba384420@gmail.com>
2024-01-10 14:14:49 +0530

    image added

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -1,12 +1,50 @@
-// components/Home.js
+// Home.js
 
 import React from "react";
+import "./Home.css";
 
 function Home() {
+  const projects = [
+    {
+      title: "Project 1",
+      description: "Description of Project 1.",
+    },
+    {
+      title: "Project 2",
+      description: "Description of Project 2.",
+    },
+    {
+      title: "Project 3",
+      description: "Description of Project 3.",
+    },
+  ];
+
   return (
-    <div>
-      <h2>Welcome to My Portfolio</h2>
-      <p>This is the home section of my portfolio.</p>
+    <div className="Home">
+      <div className="profile-section">
+        <img
+          src="images/contibutor1.png"
+          alt="My Photo"
+          className="profile-photo"
+        />
+        <h2>Hi, I'm Your Name</h2>
+        <p>
+          Welcome to my portfolio. I am a passionate developer with a focus on
+          [Your Specialization].
+        </p>
+      </div>
+
+      <div className="projects-section">
+        <h2>My Projects</h2>
+        <ul>
+          {projects.map((project, index) => (
+            <li key={index}>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
