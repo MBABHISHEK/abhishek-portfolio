566490a3d3a6f4f6058518ccb6068aad8c4215cf
ABHI <mba384420@gmail.com>
2024-01-10 14:05:24 +0530

    adding css

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,8 @@
 // App.js
 
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
@@ -12,10 +13,10 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/projects" component={Projects} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
       </div>
     </Router>
   );
